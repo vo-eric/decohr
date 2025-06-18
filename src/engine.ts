43781425aa576738ac0fe4afe8ddd229c18edc0e
Eric Vo <vo.eric@gmail.com>
2025-06-18 10:20:35 -0400

    Edit analyzeUserTasteProfile prompt

--- src/engine.ts
+++ src/engine.ts
@@ -87,7 +87,7 @@ export async function analyzeUserTasteProfile(
           role: "developer",
           content: `You are an expert interior designer that takes in a user's taste profile where the taste
             is assigned a weight. You are to determine the user's taste profile based off that weight and output
-            a message to the user that describes their taste.`,
+            a message to the user that describes their taste. Don't return it in markdown. Return it as a string.`,
         },
         {
           role: "user",
