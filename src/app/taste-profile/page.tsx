50afc9bdb9b02536a12a16c435d3f993e848b2e8
Eric Vo <vo.eric@gmail.com>
2025-06-18 15:48:30 -0400

    Style taste-profile page

--- src/app/taste-profile/page.tsx
+++ src/app/taste-profile/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { trpc } from "../_trpc/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import clsx from "clsx";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
 export default function Page() {
   const utils = trpc.useUtils();
@@ -12,20 +14,44 @@ export default function Page() {
   const tasteProfile = user?.tasteProfile;
   const likes = user?.likes as Record<string, number>;
   const [index, setIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
   const { data: generatedImages } = trpc.images.getGeneratedImages.useQuery(
     user?.id ?? "",
   );
   const generateImage = trpc.images.generateImage.useMutation({
     onSuccess: async () => {
       await utils.images.getGeneratedImages.invalidate();
+      setIndex(generatedImages!.length - 1);
     },
   });
 
-  if (!tasteProfile) {
-    return <div>No taste profile found</div>;
-  }
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        handleClick(true); // Next
+      } else if (event.key === "ArrowLeft") {
+        handleClick(false); // Previous
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
-  async function handleClick() {
+  const handleClick = (increment: boolean) => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setIsTransitioning(false);
+      if (increment) {
+        setIndex((prev) => prev + 1);
+      } else {
+        setIndex((prev) => prev - 1);
+      }
+    }, 300);
+  };
+
+  async function handleGenerateImageClick() {
     generateImage.mutate({
       userId: "11f94639-ae67-42b4-85ee-fe6cd4e4ac87",
       tasteProfile: likes,
@@ -33,29 +59,55 @@ export default function Page() {
   }
 
   return (
-    <div>
-      <p>{tasteProfile}</p>
-      <button
-        className="rounded-md bg-blue-500 p-2 text-white"
-        onClick={handleClick}
-      >
-        Generate Image
-      </button>
-      <div className="relative h-[400px] w-auto">
-        {generatedImages?.length && (
-          <Image
-            src={generatedImages[index]?.imageUrl ?? ""}
-            alt="Generated Image"
-            width={0}
-            height={0}
-            sizes="100vw"
-            fill
-            className="object-contain"
-          />
-        )}
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#c9e4ca]">
+      <div className="container grid h-full grid-cols-3 items-center justify-center gap-16">
+        <div className="col-span-1 flex h-full flex-1 flex-grow flex-col gap-8 self-start bg-[#c9e4ca] text-[#364958]">
+          <h1 className="text-4xl font-bold">Your Taste Profile</h1>
+          <p className="leading-relaxed">{tasteProfile}</p>
+          <button
+            className={clsx(
+              "mt-auto cursor-pointer rounded-md bg-[#55828b] p-2 text-white transition duration-300 hover:bg-[#42656c]",
+              generateImage.isPending && "cursor-not-allowed",
+            )}
+            onClick={handleGenerateImageClick}
+          >
+            {generateImage.isPending ? "Generating..." : "Generate Image"}
+          </button>
+        </div>
+        <div className="col-span-2 flex-2 rounded-2xl bg-[#87bba2]/20">
+          <div className="relative h-[800px] w-auto">
+            {generatedImages?.length && (
+              <Image
+                src={
+                  generatedImages[Math.abs(index) % generatedImages.length]
+                    ?.imageUrl ?? ""
+                }
+                alt="Generated Image"
+                width={0}
+                height={0}
+                sizes="100vw"
+                fill
+                className={clsx(
+                  "rounded-md object-contain px-10 transition-opacity duration-300",
+                  isTransitioning ? "opacity-0" : "opacity-100",
+                )}
+              />
+            )}
+            <button
+              className="absolute flex h-full w-[40px] cursor-pointer items-center justify-center rounded-l-2xl transition duration-300 hover:bg-[#87bba2]/10"
+              onClick={() => handleClick(false)}
+            >
+              <ChevronLeftIcon className="h-12 w-12 text-[#55828b]" />
+            </button>
+            <button
+              className="absolute right-0 flex h-full w-[40px] cursor-pointer items-center justify-center rounded-r-2xl transition duration-300 hover:bg-[#87bba2]/10"
+              onClick={() => handleClick(true)}
+            >
+              <ChevronRightIcon className="h-12 w-12 text-[#55828b]" />
+            </button>
+          </div>
+        </div>
       </div>
-      <button onClick={() => setIndex(index - 1)}>Prev</button>
-      <button onClick={() => setIndex(index + 1)}>Next</button>
-    </div>
+    </main>
   );
 }
