012d62c6f255ccae74636880d104e40a711c1681
Eric Vo <vo.eric@gmail.com>
2025-07-24 21:08:12 -0400

    Fix y padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[#c9e4ca]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4">
         {session ? (
           <ImageRatingPage userId={session.user.id} />
         ) : (
